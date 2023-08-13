2c5791f00ee4bd952dd28ebd9fde676c553618c0
Asogwa Ikechukwu Christian <ikechukwuasogwa@gmail.com>
2023-08-13 16:09:44 +0100

    init

---
+++ src/app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import joblib, os
+import pickle
+import warnings
+
+# Useful Functions
+def load_ml_components(fp):
+    'load the ml components to re-use in app'
+    with open(fp, 'rb') as f:
+        object = pickle.load(f)
+    return object
+
+# Variables and constants
+DIRPATH = os.path.dirname(os.path.realpath(__file__))
+ml_core_fp = os.path.join(DIRPATH, 'ml_Assets','ml.pkl')
+
+# Execution
+ml_components_dict = load_ml_components(fp=ml_core_fp) 
+
+labels = ml_components_dict['label']
+idx_to_labels = {i: l for (i, l) in enumerate(labels)}
+
+end2end_pipeline = ml_components_dict['pipeline']
+
+print(f"\n[Info] Predictable labels: {labels}")
+print(f"\n[Info] Predictable Idexes to labels: {idx_to_labels}")
+
+print(f"\n[Info] ML components loaded: {list(ml_components_dict.keys())}")
+
+st.write("""# TIME-SERIES REGRESSION PREDICTION APP
+         My fisrt interface using streamlit to predict a time-series regression""")
+
+# INPUTS
+date = st.date_input('Kindly Select your date')
+oil_prices = st.number_input('Enter oil price', min_value = 10.00, max_value = 1000.00)
+onpromotion = st.selectbox('Enter the promotion status on the selected date, 1 for onpromotion and 0 for no promotion]', ('1', '0'))
+transactions = st.number_input('Enter transactions Amount on the date chosen')
+
+
+# Prediction as Executed
+
+if st.button('Predict'):
+    # DataFrame creation
+    df = pd.DataFrame({
+        "date":[date],"oil_prices":[oil_prices], "transactions":[transactions], "onpromotion":[onpromotion]
+    })
+    print(f"[Info]Input data as dataframe:\n{df.to_markdown()}")
+    
+# ML PART
+output = end2end_pipeline.predict()
+
+## store confidence score/probability for the predicted class
+df['confidence score'] = output.max(axis=-1)
+
+# store index then replace by the matching label
+df['predicted label'] = predicted_idx
+predicted_label = df['predicted label'].replace(idx_to_labels)
+df['predicted label'] = predicted_label
+
+print(f'[Info] Input dataframe with prediction :\n{df.to_markdown()}')
+    
+st.text(f"The Total Sales for the chosen date is : '{predicted_label[0]}' .")
---
+++ src/appy2.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import pandas as pd
+import joblib, os
+import pickle
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+
+st.write("""# TIME-SERIES REGRESSION PREDICTION APP
+         My fisrt interface using streamlit to predict a time-series regression""")
+
+
+# Write a Subheader
+st.subheader('Kindly Enter Your Information')
+
+
+# load ml components
+
+cwd = os.getcwd()
+relative_path = "src\\ml_Assets\\ml.pkl"
+
+
+absolute_path = os.path.join(cwd, relative_path)
+print(absolute_path)
+
+#ML components Reading
+
+with open(absolute_path, "rb") as f:
+    ml_components = pickle.load(f)
+    
+labels = ml_components['label']
+pip = ml_components["pipeline"]
+#scaler = pip["scaler"]
+#model = pip["models"]
+
+# Execution
+#ml_components_dict = load_ml_components(fp=ml_core_fp) 
+
+
+#idx_to_labels = {i: l for (i, l) in enumerate(labels)}
+
+#end2end_pipeline = ml_components_dict['pipeline']
+
+# INPUTS DECLARATIONS
+
+date = st.date_input('Kindly Select your date')
+oil_prices = st.number_input('Enter oil price', min_value = 10.00, max_value = 1000.00)
+onpromotion = st.selectbox('Enter the promotion status on the selected date, 1 for onpromotion and 0 for no promotion]', ('1', '0'))
+transactions = st.number_input('Enter transactions Amount on the date chosen')
+
+
+backgroundColor = "green"
+with st.form(key = "my_form", clear_on_submit = True):
+    
+    # Output Presentation
+    submitted = st.form_submit_button("predict")
+    if submitted:
+        
+        try:
+            inputs = [['date', 'oil_prices', 'onpromotion', 'transactions'], [date, oil_prices, onpromotion, transactions]]
+            
+            df = pd.dataFrame(inputs[1:], columns = inputs[0])
+            
+            df = df.set_index('date')
+            
+            # selectingexogenous variables
+            df = df[['oil_prices', 'onpromotion']]
+            print(df)
+            
+            predicted = pip.predict(labels)
+            #labels = predicted
+            #df['Sales'] = predicted
+            
+            st.balloons()
+            st.success('Successfully Predicted', icon = '✅')
+            print(df)
+            
+            st.write(predicted)
+            st.write(df)
+            
+            
+        except:
+            st.error('something wrong has happened....', icon = '🚨')
+
+
+
+
+
+# Prediction as Executed
+
+#if st.button('predict'):
+    # DataFrame creation
+    #df = pd.DataFrame({
+        #"date":[date],"oil_prices":[oil_prices], "transactions":[transactions], "onpromotion":[onpromotion]
+    #})
+    #print(f"[Info]Input data as dataframe:\n{df.to_markdown()}")
+    
+    
+    
+    #st.text(f"The Total Sales within the period is : '{''}' .")
